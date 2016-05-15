da1e243dc260ce5386262f00efaeaa5d80f2512b
jacqk <kongfanchen1994@gmail.com>
2016-05-15 16:28:40 +0800

    2.2.4

--- chapter_2_2.scm
+++ chapter_2_2.scm
@@ -677,7 +677,7 @@
   (if (= n 0)
     painter
     (let ((smaller (right-split painter (- n 1))))
-      (beside painter (belowe smaller smaller)))))
+      (beside painter (below smaller smaller)))))
 
 (define (corner-split painter n)
   (if (= n 0)
@@ -689,4 +689,41 @@
             (corner (corner-split painter (- n 1))))
         (beside (below painter top-left)
                 (below bottom-right corner))))))
+
+; ex2.44
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let ((up (up-split painter (- n 1))))
+      (below painter (beside up up))))
+
+; higher-order operations
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (flipped-pairs painter)
+  (let ((combine4 (square-of-four identity flip-vert identity flip-vert)))
+    (combine4 painter)))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity rotatie180 flip-vert)))
+    (combine4 (corner-split painter n))))
+
+; ex2.45
+
+(define (split op1 op2)
+  (lambda (painter n)
+    (if (= n 0)
+      painter
+      (let ((smaller ((split op1 op2) painter (- n 1))))
+        (op1 painter
+             (op2 smaller smaller))))))
+
+; framework
+
+
 
