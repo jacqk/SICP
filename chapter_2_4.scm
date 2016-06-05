6b88ca089f2c6d4728a70ae9445a6a4a17422e78
jacqk <kongfanchen1994@gmail.com>
2016-06-05 13:58:45 +0800

    2.4

--- chapter_2_4.scm
+++ chapter_2_4.scm
@@ -339,6 +339,32 @@
 (newline)
 (display (deriv expr-2 'x))
 
-; ex2.74
+; ex2.74 unsolved
 
+; p127
+
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+            (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+(define (apply-generic op arg) (arg op))
+
+; ex2.75
+
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)))
+  dispatch)
+
+; ex2.76
 
